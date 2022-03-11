b8380c9f3d89a51db5ab9dae1f63dd880be42885
matheuscamarques <matheuscamarques@gmail.com>
2022-03-11 00:28:20 -0300

    ajustes em arquivos identaçao

--- lib/finance_chain/services/blockchain_service.ex
+++ lib/finance_chain/services/blockchain_service.ex
@@ -29,24 +29,28 @@ defmodule FinanceChain.Services.BlockChain do
       when wallet.origin == 0 and wallet.destination != 0 and wallet.signature == "deposit" do
     BlockChain.add_block(wallet)
     amount = BlockChain.get_balance(wallet.destination)
-    {:ok, %{destination: %{balance: amount, id: wallet.destination,}}}
+    {:ok, %{destination: %{id: wallet.destination, balance: amount}}}
   end
 
   def deposit_into_existing_account(%Wallet{} = wallet)
       when wallet.origin == 0 and wallet.destination != 0 and wallet.signature == "deposit" do
     BlockChain.add_block(wallet)
     amount = BlockChain.get_balance(wallet.destination)
-    {:ok, %{destination: %{balance: amount,id: wallet.destination}}}
+    {:ok, %{destination: %{id: wallet.destination, balance: amount}}}
   end
 
   def deposit(%Wallet{} = wallet)
       when wallet.origin == 0 and wallet.destination != 0 and wallet.signature == "deposit" do
     BlockChain.add_block(wallet)
     amount = BlockChain.get_balance(wallet.destination)
-    {:ok, %{destination: %{ balance: amount , id: wallet.destination,}}}
+    {:ok, %{destination: %{id: wallet.destination, balance: amount}}}
   end
 
-
+  def withdraw(%Wallet{} = wallet) when wallet.signature == "withdraw" do
+    blockchain = BlockChain.get_blockchain()
+    id = Utils.search(blockchain, wallet.origin)
+    withdraw(id, wallet)
+  end
 
   def withdraw_from_non_existing_account(%Wallet{} = wallet)
       when wallet.signature == "withdraw" do
@@ -61,18 +65,12 @@ defmodule FinanceChain.Services.BlockChain do
     withdraw(id, wallet)
   end
 
-  def withdraw(%Wallet{} = wallet) when wallet.signature == "withdraw" do
-    blockchain = BlockChain.get_blockchain()
-    id = Utils.search(blockchain, wallet.origin)
-    withdraw(id, wallet)
-  end
-
   defp withdraw(0, _), do: {:err, 0}
 
   defp withdraw(id, wallet) when id > 0 do
     BlockChain.add_block(wallet)
     amount = BlockChain.get_balance(wallet.origin)
-    {:ok, %{origin: %{balance: amount , id: wallet.origin}}}
+    {:ok, %{origin: %{id: wallet.origin, balance: amount}}}
   end
 
   def transfer_from_existing_account(%Wallet{} = wallet) when wallet.signature == "transfer" do
@@ -104,8 +102,8 @@ defmodule FinanceChain.Services.BlockChain do
 
     {:ok,
      %{
-       destination: %{ balance: destination_amount, id: wallet.destination},
-       origin: %{ balance: origin_amount , id: wallet.origin}
+       origin: %{id: wallet.origin, balance: origin_amount},
+       destination: %{id: wallet.destination, balance: destination_amount}
      }}
   end
 end
